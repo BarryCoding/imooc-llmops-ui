1c5a528ed6c245744068b18aba7d93d54744e00e
AIMan <zxllks@yahoo.com>
2025-11-09 09:04:11 +0800

    feat: add routing and layout components for authentication and app listing

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,8 +1,34 @@
 import { createRouter, createWebHistory } from 'vue-router'
+import BlankLayout from '@/views/layouts/BlankLayout.vue'
+import DefaultLayout from '@/views/layouts/DefaultLayout.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
-  routes: [],
+  routes: [
+    {
+      path: '/',
+      component: DefaultLayout,
+      children: [
+        {
+          path: 'space/apps',
+          name: 'space.apps.list',
+          component: () => import('@/views/space/apps/ListView.vue'),
+        },
+      ],
+    },
+
+    {
+      path: '/',
+      component: BlankLayout,
+      children: [
+        {
+          path: 'auth/login',
+          name: 'auth.login',
+          component: () => import('@/views/auth/LoginView.vue'),
+        },
+      ],
+    },
+  ],
 })
 
 export default router
---
+++ src/views/space/apps/ListView.vue
@@ -0,0 +1,5 @@
+<template>
+  <p>这里是AI应用列表</p>
+  <p>数值是</p>
+  <RouterLink to="/home">返回主页</RouterLink>
+</template>
